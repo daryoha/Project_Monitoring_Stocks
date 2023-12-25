1427682269c953aa10aee5d8700b446ae1386d31
daryoha <dastrelkova@edu.hse.ru>
2023-12-26 02:46:00 +0300

    finally works

--- header.py
+++ header.py
@@ -20,11 +20,12 @@ from telebot.async_telebot import AsyncTeleBot
 from datetime import date, timedelta
 data_days = pd.read_csv('fin_quotes_per_day.csv')
 CLIENT_ID="f45d25c17f9968c"
-TOKEN = '6749218741:AAFJoqLXOhZvh2kYp617miRKUiYuQHFFUvE'
+TOKEN = '6759361009:AAH1NZP6Xjk92bVCLEKUcpOUrFhw8WhYBFA'
 bot = AsyncTeleBot(TOKEN)
 comp_list=["Yandex", "Газпром", "Лукойл", "Сбербанк", "Русал", "Интер РАО", "Норильский никель", "Магнит", "МТС"]
 comp_dict={"Yandex":"YNDX", "Газпром":"GAZP", "Лукойл":"LKOH", "Сбербанк":"SBER", "Русал":"RUAL", "Интер РАО":"IRAO",
-           "Норильский никель":"GMKN", "Магнит":"MGNT", "МТС":"MTSS"}
+           "Норильский никель":"GMKN", "Магнит":"MGNT", "МТС":"MTSS", "Татнефть":'TATN', "Сургутнефтегаз":'SNGS',
+           "НОВАТЭК":'NVTK', "Полюс":'PLZL', "Северсталь":'CHMF'}
 comp_list_pred=["Яндекс", "Газпром", "Лукойл", "Сбербанк", "Русал", "Интер РАО", "Норильский никель",
                 "Магнит", "МТС", "Полюс", "Роснефть", "Северсталь", "Сургутнефтегаз", "НОВАТЭК", "Татнефть"]
 quotes_day=pd.read_csv("fin_quotes_per_day.csv")
--- main.py
+++ main.py
@@ -32,38 +32,6 @@ async def main():
       markup.add(btn1)
       await bot.send_message(message.from_user.id, "Здравствуйте! Этот бот поможет вам отслеживать котировки акций.", reply_markup=markup)
 
-  @bot.poll_answer_handler(func=lambda call: True)
-  async def handle_poll_answer(pollAnswer):
-      ops=pollAnswer.option_ids
-      res=[]
-      msg = types.Message(message_id=0, from_user=pollAnswer.user.id, date='', chat = pollAnswer.user.id, content_type='text', options=[], json_string='')
-      if choice[pollAnswer.user.id]=="Котировки":
-          for x in ops:
-              res.append(comp_list[x])
-          #
-          new_r = pickle.dumps(res)
-          conn = sqlite3.connect('it_user.sql')
-          cur = conn.cursor() # ссылка на контекстную область памяти #работае синх асинх
-          cur.execute('UPDATE users SET companies_cot = ? WHERE id = ?', (new_r, pollAnswer.user.id))
-          conn.commit() #синхроним
-          cur.close()
-          conn.close()
-          #
-          msg.text='Компании для котировок'
-      elif choice[pollAnswer.user.id]=="Предсказания":
-          for x in ops:
-              res.append(comp_dict[comp_list[x]])
-          #
-          new_r = pickle.dumps(res)
-          conn = sqlite3.connect('it_user.sql')
-          cur = conn.cursor() # ссылка на контекстную область памяти #работае синх асинх
-          cur.execute('UPDATE users SET companies_info = ? WHERE id = ?', (new_r, pollAnswer.user.id))
-          conn.commit() #синхроним
-          cur.close()
-          conn.close()
-          msg.text ='Компании для прогноза'
-      await get_messages(msg)
-
   @bot.message_handler(content_types=['text', "test"])
   async def get_messages(message):
       if message.text == "Отлично! Начнем работу :)":
@@ -129,18 +97,7 @@ async def main():
           await bot.send_message(message.from_user, 'Выберите временной промежуток', reply_markup=markup)
 
       elif message.text.split()[0][:7]=="Последн":
-          #
           user_id = message.from_user.id
-          # conn = sqlite3.connect('it_user.sql')
-          # cur = conn.cursor() # ссылка на контекстную область памяти #работае синх асинх
-          # cur.execute('SELECT * FROM users WHERE id = ?', (user_id,))
-          # table = cur.fetchall()
-
-          # user_cot = table[0][1]
-          # cur.close()
-          # conn.close()
-          # user_cot = pickle.loads(user_cot)
-          # #Высчитываем временной промежуток
           end_date = date.today().isoformat()
           start_date=0
           time_range=message.text.split()[1]
@@ -150,16 +107,12 @@ async def main():
           elif (time_range=="месяц"):
                start_date = (date.today()-timedelta(days=30)).isoformat()
           data_user=data_days[data_days["Date"]>=start_date]
-          print(header.company)
           comp=data_user[["Date", header.company]]
-          print(0)
           text=(f'Компания: {header.company}\n'
                 f'Период: {time_range}\n')
-          print(1)
           plt.figure(figsize=(6, 4))
           line=sns.lineplot(data=comp, x='Date', y=header.company)
           line.set(xlabel='Date', ylabel='Stock price')
-          print(2)
           line.set_title(f'Stock price dynamics for {header.company} company', fontdict={'size': 10, 'weight': 'bold', 'color': 'green', 'style':'italic'})
           plt.xticks(rotation=90)
           filename=f'{header.company}.png'
@@ -174,19 +127,7 @@ async def main():
           await bot.send_message(user_id, text)
           await bot.send_photo(user_id, uploaded_image.link, reply_markup=markup)
           os.remove(filename)
-      elif message.text=="Компании для прогноза":
-          #
-          user_id = message.from_user
-          conn = sqlite3.connect('it_user.sql')
-          cur = conn.cursor() # ссылка на контекстную область памяти #работае синх асинх
-          cur.execute('SELECT * FROM users WHERE id = ?', (user_id))
-          table = cur.fetchall()
-          user_info = table[0][2]
-          cur.close()
-          conn.close()
-          user_info = pickle.loads(user_info)
 
-          notif_comp_user=user_info
   await bot.polling(none_stop=True, interval=0)
 
 asyncio.run(main())
